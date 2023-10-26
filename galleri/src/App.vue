d6879f139b280515359332d6e9c6cbc7c8ad91e5
joyalzzy <joyalzzy@proton.me>
2023-10-27 00:05:43 +0800

    initial changes

--- galleri/src/App.vue
+++ galleri/src/App.vue
@@ -1,116 +1,60 @@
 <template>
     <!-- <button v-on:click="updateSlides">Add new image</button> -->
-    <lightgallery :settings="{ speed: 500, plugins: plugins }" :onInit="onInit" :onBeforeSlide="onBeforeSlide">
-        <a v-for="item in items" :key="item.id" className="gallery-item" :data-src="item.src">
-            <!-- @vue-ignore -->
-            <img className="img-responsive" :src="item.thumb" />
-        </a>
-    </lightgallery>
+    <div id="gallery" data-nanogallery2></div>
 </template>
 
 <script lang="ts">
-import Lightgallery from 'lightgallery/vue';
-import lgZoom from 'lightgallery/plugins/zoom';
-import lgVideo from 'lightgallery/plugins/video';
 import axios from 'axios';
 import { stringifyQuery } from 'vue-router';
 import { defineComponent } from 'vue';
+import $ from 'jquery';
+import 'nanogallery2';
 
 
-let lightGallery: any = null;
 console.log(import.meta.env.VITE_API_URL)
-const links = await axios.get(`${import.meta.env.VITE_API_URL ?? "http://localhost:3000/v1"}/links`).then(
-    res => res.data
-).then(
-    res => {
-        return res as String[]
-    }
-)
+// const links = await axios.get(`${import.meta.env.VITE_API_URL ?? "http://localhost:3000/v1"}/links`).then(
+    // res => res.data
+// ).then(
+    // res => {
+        // return res as String[]
+    // }
+// )
 export default defineComponent({
-    name: 'App',
-    components: {
-        Lightgallery,
-    },
-    watch: {
-        items(newVal, oldVal) {
-            this.$nextTick(() => {
-                lightGallery.refresh();
-            });
-        },
-    },
-    data: () => ({
-        plugins: [lgZoom, lgVideo],
-        items: links.map((a, i) => {
-            if (!a.includes("mp4")) {
-                return {
-                    id: (i + 2).toString(),
-                    src: a,
-                    thumb: a
-                }
-            }
-            else {
-                return {
-                    id: (i + 2).toString(),
-                    src: a,
-                    thumb: a,
-                    type: 'video/mp4'
-                }
-            }
-        })
-    })
-    // [
-    // {
-    // id: '1',
-    // size: '1400-933',
-    // src:
-    // 'https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80',
-    // thumb:
-    // 'https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80',
-    // subHtml: `<div class="lightGallery-captions">
-    // <h4>Photo by <a href="https://unsplash.com/@dann">Dan</a></h4>
-    // <p>Published on November 13, 2018</p>
-    // </div>`,
+    // name: 'App',
+    // components: {
     // },
-    // {
-    // id: '2',
-    // size: '1400-933',
-    // src:
-    // 'https://images.unsplash.com/photo-1473876988266-ca0860a443b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80',
-    // thumb:
-    // 'https://images.unsplash.com/photo-1473876988266-ca0860a443b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80',
-    // subHtml: `<div class="lightGallery-captions">
-    // <h4>Photo by <a href="https://unsplash.com/@kylepyt">Kyle Peyton</a></h4>
-    // <p>Published on September 14, 2016</p>
-    // </div>`,
-    // },
-    // {
-    // id: '3',
-    // size: '1400-932',
-    // src:
-    // 'https://images.unsplash.com/photo-1588953936179-d2a4734c5490?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80',
-    // thumb:
-    // 'https://images.unsplash.com/photo-1588953936179-d2a4734c5490?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80',
-    // subHtml: `<div class="lightGallery-captions">
-    // <h4>Photo by <a href="https://unsplash.com/@jxnsartstudio">Garrett Jackson</a></h4>
-    // <p>Published on May 8, 2020</p>
-    // </div>`,
-    // },
-    // ],
-    , methods: {
-        onInit: (detail: any) => {
-            lightGallery = detail.instance;
-        },
-        updateSlides: function () {
-            this.items = [
-                ...this.items,
-            ]
-            lightGallery.refresh();
-        },
-        onBeforeSlide: () => {
-            console.log('calling before slide');
-        },
-    },
+    // data: () => ({
+        // items: links.map((a, i) => {
+            // if (!a.includes("mp4")) {
+                // return {
+                    // src: a,
+                    // scrt: a
+                // }
+            // }
+            // else {
+                // return {
+                    // src: a,
+                    // scrt: 'video/mp4'
+                // }
+            // }
+        // })
+    // })
 });
+$(document).ready(function () {
+    $("#gallery").nanogallery2({
+      items: [
+        {
+          src:          'https://vimeo.com/32875422',                           // media url
+          srct:         'https://i.vimeocdn.com/video/222726041_1280x720.jpg',  // media thumbnail url
+          title:        'Vimeo video',                                          // media title
+          description:  'Video'                                                 // media description
+        },
+        { src: 'https://www.youtube.com/watch?v=Ir098VWCv8Q', title: 'Youtube video' },
+        { src: 'http://www.dailymotion.com/video/x4wtyl6',    title: 'Dailymotion video' },
+        { src: 'image_01.jpg',      srct: 'image_01ts.jpg',   title: 'Title Image' }
+      ]
+    });
+  });
 </script>
 <style lang="css">
 @import url('https://cdn.jsdelivr.net/npm/lightgallery@2.1.0-beta.1/css/lightgallery.css');
