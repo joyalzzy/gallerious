f38710b221a09b8cc3613e1d86ecbb958f84b2b0
joyalzzy <joyalzzy@proton.me>
2023-10-27 10:09:41 +0800

    removed everything

--- galleri/src/App.vue
+++ galleri/src/App.vue
@@ -1,14 +1,21 @@
 <template>
-    <!-- <button v-on:click="updateSlides">Add new image</button> -->
-    <div id="gallery" data-nanogallery2></div>
+    <h3>gallery ig</h3>
+    <lightgallery
+        :settings="{ speed: 500, plugins: plugins }"
+    >
+        
+        
+    </lightgallery>
 </template>
 
 <script lang="ts">
+import Lightgallery from 'lightgallery/vue';
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
 import axios from 'axios';
 import { stringifyQuery } from 'vue-router';
 import { defineComponent } from 'vue';
 import $ from 'jquery';
-import 'nanogallery2';
 
 
 console.log(import.meta.env.VITE_API_URL)
@@ -20,10 +27,11 @@ console.log(import.meta.env.VITE_API_URL)
     // }
 // )
 export default defineComponent({
-    // name: 'App',
-    // components: {
-    // },
-    // data: () => ({
+    name: 'App',
+    components: {
+    },
+    data: () => ({
+        plugins: [lgThumbnail, lgZoom],
         // items: links.map((a, i) => {
             // if (!a.includes("mp4")) {
                 // return {
@@ -38,23 +46,11 @@ export default defineComponent({
                 // }
             // }
         // })
-    // })
+        items: [{
+            src: "https://media.discordapp.net/attachments/1140180380683612160/1140180381652484147/huge_2023-08-13_15.09.15.png?ex=65473fba&is=6534caba&hm=63bca0e9e8b1b3b838280ec6fb16626f1928c5f78e0916b9e35d3a080dad9141&=&width=1058&height=595"
+        }]
+    })
 });
-$(document).ready(function () {
-    $("#gallery").nanogallery2({
-      items: [
-        {
-          src:          'https://vimeo.com/32875422',                           // media url
-          srct:         'https://i.vimeocdn.com/video/222726041_1280x720.jpg',  // media thumbnail url
-          title:        'Vimeo video',                                          // media title
-          description:  'Video'                                                 // media description
-        },
-        { src: 'https://www.youtube.com/watch?v=Ir098VWCv8Q', title: 'Youtube video' },
-        { src: 'http://www.dailymotion.com/video/x4wtyl6',    title: 'Dailymotion video' },
-        { src: 'image_01.jpg',      srct: 'image_01ts.jpg',   title: 'Title Image' }
-      ]
-    });
-  });
 </script>
 <style lang="css">
 @import url('https://cdn.jsdelivr.net/npm/lightgallery@2.1.0-beta.1/css/lightgallery.css');
